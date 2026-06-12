9d66c9b1360c08221422e57d4317fdda2e33c830
Harikiran <53599889+hari-ghk@users.noreply.github.com>
2026-06-12 23:10:47 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+    
+        if(hand.length % groupSize != 0)
+            return false;
+
+        TreeMap<Integer, Integer> countMap = new TreeMap<>();
+        for(int i = 0; i < hand.length; ++i){
+            countMap.put(hand[i], countMap.getOrDefault(hand[i], 0) + 1);
+        }
+
+        int currFormed = 0;
+        while(countMap.size() != 0){
+            int startPoint = countMap.firstKey(); 
+            int term = startPoint + groupSize - 1;
+            while(startPoint <= term){
+                if(!countMap.containsKey(startPoint)){
+                    return false;
+                }
+                else {
+                    countMap.put(startPoint, countMap.get(startPoint)-1);
+                    if(countMap.get(startPoint) == 0)
+                        countMap.remove(startPoint);
+                }
+                ++startPoint;
+            }
+        }
+        return true;
+    }
+}
